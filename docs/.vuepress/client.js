36d08f856f8a16933b265d6f38aee6ac9d38d466
wuyang <1161547386@qq.com>
2025-03-25 19:45:34 +0800

    fix

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,5 +1,5 @@
 import { defineClientConfig } from '@vuepress/client';
-import PhotoScroller from '../../src/components/Photo-scroller/index.tsx';
+import PhotoScroller from '../../src/components/Photo-scroller/Photo-scroller.vue';
 
 export default defineClientConfig({
   enhance({ app }) {
